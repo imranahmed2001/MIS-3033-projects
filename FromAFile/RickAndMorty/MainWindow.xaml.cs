a49a6621d3a1554e5df593f9929460d64cfb7d37
Imran Ahmed <ahmed.imran01@ou.edu>
2022-03-27 18:21:19 -0500

    Finished rick and morty api

---
+++ FromAFile/RickAndMorty/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RickAndMorty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string url = "https://rickandmortyapi.com/api/character";
+            RickAndMortyAPI api;
+
+            do
+            {
+                using (var client = new HttpClient())
+                {
+                    string json = client.GetStringAsync(url).Result;
+
+                    api = JsonConvert.DeserializeObject<RickAndMortyAPI>(json);
+
+                    foreach (var character in api.results)
+                    {
+                        cboCharacters.Items.Add(character);
+                    }
+                }
+                url = api.info.next;
+
+            } while (api.info.next != null);
+        }
+
+        private void cboCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Character selected = (Character) cboCharacters.SelectedItem;
+
+            imgCharacter.Source = new BitmapImage(new Uri(selected.image));
+        }
+    }
+}
