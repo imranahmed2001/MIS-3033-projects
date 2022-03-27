5b78a900c95a94413f2893d812e9238a1ac3dba9
Imran Ahmed <ahmed.imran01@ou.edu>
2022-03-27 16:20:59 -0500

    finished json from a file

---
+++ FromAFile/API_Practice/CarDataAPI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Practice
+{
+    public class CarDataAPI
+    {
+        public string FirstName { get; set; }
+
+        public string LastName  {get; set;}
+
+        public string Make      {get; set;}
+
+        public string Model     {get; set;}
+
+        public int Year      {get; set;}
+
+        public string Color     {get; set;}
+
+        public CarDataAPI()
+        {
+            FirstName = String.Empty;
+            LastName = String.Empty;
+            Make = String.Empty;
+            Model = String.Empty;
+            Year = 0;
+            Color = String.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} has {Make} {Model} made in {Year}";
+        }
+    }
+}
+            
---
+++ FromAFile/API_Practice/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace API_Practice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<CarDataAPI> cardata = new List<CarDataAPI> ();
+        public MainWindow()
+        {
+            InitializeComponent();
+            cboColor.Items.Add("ALL");
+
+            string json = File.ReadAllText("Mock_Data_Car_Owners.json");
+
+            cardata = JsonConvert.DeserializeObject<List<CarDataAPI>>(json);
+
+            foreach (CarDataAPI item in cardata)
+            {
+                if (cboColor.Items.Contains(item.Color) == false)
+                {
+                    cboColor.Items.Add(item.Color);
+                }
+            }
+
+        }
+
+        private void cboColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lstCarInfo.Items.Clear();
+            string color = cboColor.SelectedItem.ToString();
+
+            foreach (CarDataAPI item in cardata)
+            {
+                if (color == "ALL")
+                {
+                    lstCarInfo.Items.Add(item);
+                }
+                else if (color == item.Color)
+                {
+                    lstCarInfo.Items.Add(item);
+                }
+            }
+            RecordCount.Content = $"Record Count: {lstCarInfo.Items.Count}";
+        }
+    }
+}
