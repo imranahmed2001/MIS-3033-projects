96d75ce79b81d0b2b7585c696fa0fa2d80552481
Imran Ahmed <ahmed.imran01@ou.edu>
2022-03-27 19:20:16 -0500

    FInished GOT quote API

---
+++ FromAFile/GameOfThronesQuotes/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameOfThronesQuotes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnQuote_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://got-quotes.herokuapp.com/quotes";
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+
+                GOTAPI api = JsonConvert.DeserializeObject<GOTAPI>(json);
+
+                string quote = $"{api.character}:\n {api.quote}";
+
+                lblQuote.Content = quote;
+            }
+        }
+    }
+}
